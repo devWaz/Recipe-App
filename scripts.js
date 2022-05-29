b9ef4be58937acc09a725b129a10df9f1a3de1ed
devWaz <fawazodessy@gmail.com>
2022-05-29 23:11:32 +0100

    Responsive

--- scripts.js
+++ scripts.js
@@ -1,4 +1,23 @@
 const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
+const mealDetailsContent = document.querySelector('.meal-details-content');
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
-console.log(mealList);
+// Event listeners
+searchBtn.addEventListener('click', getMealList);
+mealList.addEventListener('click', getMealRecipe);
+recipeCloseBtn.addEventListener('click', () => {
+    mealDetailsContent.parentElement.classList.remove('showRecipe');
+});
+
+function getMealList(){
+    let searchInputTxt = document.getElementById('search-input').value.trim();
+    console.log(searchInputTxt)
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+    .then(response => {console.log(response.json());})
+    // .then(data =>{
+    //     console.log(data)
+    // })
+
+    
+}
