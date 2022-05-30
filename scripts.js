c7ff20cae7807f9ed0ee6501aaee6909ec174292
devWaz <fawazodessy@gmail.com>
2022-05-30 09:17:25 +0100

    Recipes

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-const searchBtn = document.getElementById('search-btn');
-const mealList = document.getElementById('meal');
-const mealDetailsContent = document.querySelector('.meal-details-content');
+var searchBtn = document.getElementById('search-btn');
+var mealList = document.getElementById('meal');
+var mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
 // Event listeners
@@ -15,9 +15,28 @@ function getMealList(){
     console.log(searchInputTxt)
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
     .then(response => {console.log(response.json());})
-    // .then(data =>{
-    //     console.log(data)
-    // })
+    .then(data => {
+        let searchResults = "";
+        if(data.meals){
+            data.meals.forEach(meal => {
+                searchResults += `
+                    <div class = "meal-item" data-id = "${meal.idMeal}">
+                        <div class = "meal-img">
+                            <img src = "${meal.strMealThumb}" alt = "food">
+                        </div>
+                        <div class = "meal-name">
+                            <h3>${meal.strMeal}</h3>
+                            <a href = "#" class = "recipe-btn">Get Recipe</a>
+                        </div>
+                    </div>
+                `;
+            });
+            mealList.classList.remove('notFound');
+        } else{
+            searchResults = "Sorry, we didn't find any meal!";
+            mealList.classList.add('notFound');
+        }
 
-    
+        mealList.innerHTML = searchResults;
+    });
 }
